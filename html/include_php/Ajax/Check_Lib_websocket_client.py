5d25daa40e48f81478efebbfc98f491c20b6f22e
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2024-12-20 23:27:43 +0700

    Delete html directory

--- html/include_php/Ajax/Check_Lib_websocket_client.py
+++
@@ -1,9 +0,0 @@
-import pkgutil
-
-def is_websocket_client_installed():
-    return pkgutil.find_loader("websocket") is not None
-
-if is_websocket_client_installed():
-    print("Thư viện websocket-client đã được cài đặt.")
-else:
-    print("websocket-client_library_is_not_installed")
--- html/library/README.md
+++
@@ -1,7 +0,0 @@
-TESTER
-
-Kiểm tra và cài thư viện pip tự động cho Vietbot
-
-
-    $: cd /home/pi/vietbot_offline/html/library
-    $: python3 install.py
