e6d92c150a037d9a7433bf225f85ca4939938541
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2024-12-25 08:54:50 +0700

    Update start.py

--- raspberry/64/start.py
+++ raspberry/64/start.py
@@ -10,7 +10,7 @@ from api_process import app  # Import ứng dụng Quart từ api_process.py
 async def main():
     # Run tasks
     tasks = [
-        asyncio.create_task(loop(process)),  # Vòng lặp chính
+        asyncio.create_task(loop(process, False)),  # Chạy vòng lặp chính
         asyncio.create_task(app.run_task(host="0.0.0.0", port=constant.web_port)),  # API server task
         
     ]
