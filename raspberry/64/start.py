a5c6aa12f81978b8f3c16210891a54d585002f60
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2024-12-26 10:21:03 +0700

    Update start.py

--- raspberry/64/start.py
+++ raspberry/64/start.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pythonf
 # -*- coding: utf-8 -*-
-from lib_process import constant, global_vars, asyncio
+from lib_process import global_constants, global_vars, asyncio
 import init
 from loop_process import loop
 from callback_process import process
@@ -9,7 +9,7 @@ async def main():
     # Run tasks
     tasks = [
         asyncio.create_task(loop(process, False)),  # Chạy vòng lặp chính
-        asyncio.create_task(app.run_task(host="0.0.0.0", port=constant.web_port)),  # Chạy vòng lặp lắng nghe API
+        asyncio.create_task(app.run_task(host="0.0.0.0", port=global_constants.web_port)),  # Chạy vòng lặp lắng nghe API
     ]
     try:
         # Gather tasks and wait for completion
