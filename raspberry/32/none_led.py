686af16654b92f4af2e5833ac7b622627a0bbceb
Nguyễn Duy <64348125+phanmemkhoinghiep@users.noreply.github.com>
2024-12-27 12:38:04 +0700

    Add files via upload

---
+++ raspberry/32/none_led.py
@@ -0,0 +1,34 @@
+ 
+class Led():    
+    def __init__(self):
+        pass
+    def set_state(self, state):
+        pass                                   
+    def get_state(self):
+        return None             
+    def off(self):
+        pass
+    def wakeup(self):
+        pass
+    def mute(self):
+        pass
+    def listen(self):
+        pass                          
+    def think(self):
+        pass                          
+    def speak(self):
+        pass                       
+    def volume_change(self, volume1, volume2):
+        pass     
+# if __name__ == '__main__':
+    # led.speak()
+    # print(colored('[SPEAK ' +datetime.today().strftime("%d/%m/%Y")+" "+datetime.now().strftime("%H:%M:%S")+']:','green'))
+    # libs.time.sleep(2)
+    
+
+    
+    # led.off()  
+    # print(colored('[OFF ' +datetime.today().strftime("%d/%m/%Y")+" "+datetime.now().strftime("%H:%M:%S")+']:','green'))    
+    # libs.time.sleep(3)
+    
+    # print(colored('[FINISH ' +datetime.today().strftime("%d/%m/%Y")+" "+datetime.now().strftime("%H:%M:%S")+']:','green'))    
